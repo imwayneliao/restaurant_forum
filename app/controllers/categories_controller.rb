69109f7f96252bb0a0ea36db184a548e8791e5d3
imwayneliao <imwayneliao@gmail.com>
2017-12-28 23:12:34 +0800

    user on index page can select restaurants by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
-  def index
+  def show
     @categories = Category.all
+    @category = Category.find(params[:id])
+    @restaurants = @category.restaurants.page(params[:page]).per(9)
   end
 end
