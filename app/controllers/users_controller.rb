8eadc46edf5ebffd58514bf2e1b74a584756d562
imwayneliao <imwayneliao@gmail.com>
2017-12-31 16:57:53 +0800

    users can update their profile including avatars

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+      flash[:notice] = "user was successfully updated"
+    else
+      render :edit
+      flash[:alert] = "user was failed to update"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :intro, :tel, :avatar)
+  end
 end
