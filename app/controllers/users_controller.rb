98fa180803d72b50343b75336fd890bc9686b211
imwayneliao <imwayneliao@gmail.com>
2018-01-08 21:50:54 +0800

    users can view their commented_restaurants in their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @commented_restaurants = @user.restaurants
   end
   def edit
     @user = User.find(params[:id])
     redirect_to user_path(@user) if @user != current_user
-
   end
   def update
     @user = User.find(params[:id])
