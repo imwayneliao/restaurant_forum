65b34ac27f09576d8126521a3a75e9693853b615
imwayneliao <imwayneliao@gmail.com>
2018-01-13 17:39:14 +0800

    create favorite model and setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+   # 「使用者收藏很多餐廳」的多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
   def admin?
