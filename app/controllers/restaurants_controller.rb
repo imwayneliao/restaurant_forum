9125e1b5600d90fc70a4f785cb9fe626cfc76301
imwayneliao <imwayneliao@gmail.com>
2018-01-19 09:12:50 +0800

    add top10 ranking restaurant page and ranking controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,6 +12,9 @@ class RestaurantsController < ApplicationController
     @recent_restaurants = Restaurant.order(created_at: :desc).limit(10)
     @recent_comments = Comment.order(created_at: :desc).limit(10)
   end
+  def ranking
+    @top_restaurants = Restaurant.order(favorites_count: :desc).limit(10)
+  end
   def dashboard
     @restaurant = Restaurant.find(params[:id])
   end
