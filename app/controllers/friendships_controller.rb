382a165819e82fab8e92ddf29876be6d6aa3e309
imwayneliao <imwayneliao@gmail.com>
2018-01-21 22:34:23 +0800

    add destroy methd to friendships controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,11 +2,17 @@ class FriendshipsController < ApplicationController
   def create
     @friendship = current_user.friendships.build(friend_id: params[:friend_id])
     if @friendship.save
-      flash[:notice] = "Successfully followed"
+      flash[:notice] = "Successfully friended"
       redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @friendship.errors.full_messages.to_sentence
       redirect_back(fallback_location: root_path)
     end
+  end
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id])
+    @friendship.destroy_all
+    flash[:alert] = "Friendship destroyed"
+    redirect_back(fallback_location: root_path)
   end
 end
