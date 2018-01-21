17013c30f58d6e625060caeb4fb78242dbf8b979
imwayneliao <imwayneliao@gmail.com>
2018-01-21 22:41:03 +0800

    user has many inverse_friendships and inverse_friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
   has_many :followers, through: :inverse_followships, source: :user
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
   validates_presence_of :name
   mount_uploader :avatar, AvatarUploader
   def admin?
